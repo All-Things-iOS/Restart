0ae386d9707a0a088c925d0b0eda9e97c244e5d7
deepak <deepakkumardash313@gmail.com>
2022-04-13 21:03:19 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Deepak kumar Dash on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
