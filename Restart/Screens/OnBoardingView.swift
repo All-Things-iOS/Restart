2005568eaea6e77433ec1285a6f32951bc520a69
deepak <deepakkumardash313@gmail.com>
2022-04-13 21:37:17 +0530

    add : onboarding & home view along with button navigation using appstorage

---
+++ Restart/Screens/OnBoardingView.swift
@@ -0,0 +1,31 @@
+//
+//  OnBoardingView.swift
+//  Restart
+//
+//  Created by Deepak kumar Dash on 13/04/22.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        }
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
